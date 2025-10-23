6ec3d6407d56156e958da037d7e3fbe3011f548d
felipesfl <felipeferreira.desenvolvedor@gmail.com>
2025-10-23 10:41:50 -0300

    codigo de leitura da planilha - commit 02

--- index.py
+++ index.py
@@ -3,3 +3,18 @@ import gspread
 from gspread_dataframe import _dataframe
 from bpmn_python.bpmn_diagram_rep import BpmnDiagramGraph
 
+account_file = "credenciais.json"
+planilhas_url = "https://docs.google.com/spreadsheets/d/1KNju73dwOmkf-v3_s2kVVdo0W7isi6Gv/edit?usp=sharing"
+fluxograma_file = "fluxo_automatizado.bpmn"
+
+print("Conectando á planilha...")
+
+gc = gspread.service_account(filename=account_file)
+sh = gc.open_by_url(planilhas_url)
+worksheet = sh.sheet1
+
+df = get_as_dataframe(worksheet, evaluate_formulas = True).dropha(how = "all")
+
+print("Planilha carregada com sucesso")
+print(df.head())
+
